3d199dbf2320e2141367147f5d1c40352aefa3bb
Wagner <wagneralexandre841@gmail.com>
2020-10-22 22:41:13 -0300

    feito o leiaut

---
+++ PrimeiroProjeto/controllers/site/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* Controller do site.
+*
+* @author Code Universe
+*
+**/
+class SiteController extends Controller
+{
+
+	public function index()
+	{		
+
+		$this->view('site/home/index.php');
+
+	}
+
+}
---
+++ PrimeiroProjeto/views/site/home/index.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Primeiro Projeto MVC</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="assets/libs/bootstrap/bootstrap.css">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class=" d-flex h-100 w-100 mx-auto flex-column">
+      <header class="masthead mb-auto p-3">
+        <div class="inner container">
+          <h3 class="masthead-brand">logo</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main">
+        <section class="banner">
+           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="assets/img/anime2.jpg" class="d-block w-100" alt="...">
+                </div>
+                  <div class="carousel-item">
+                    <img src="assets/img/anime1.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/img/anime3.jpg" class="d-block w-100" alt="...">
+                  </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+              </a>
+          </div>
+        </section>
+        <section class="section2 m-5">
+          <h2 class="mb-3">section 2</h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-4"><div class="imagem" class="imagem1"></div></div>
+              <div class="col-4"><div class="imagem" class="imagem2"></div></div>
+              <div class="col-4"><div class="imagem" class="imagem3"></div></div>
+            </div>
+          </div>
+        </section>
+        <section class="section3 m-5">
+          <h2 class="mb-3">section 2</h2>
+          <div class="container">
+            <div class="row justify-md-center">
+              <div class="col-10">
+                <p>
+                  É uma forma de interação social, podem ter características faladas ou escritas, tamanhos diversificados. Por meio de um objetivo gira em torno de idéias e sentidos, se expressa de forma verbal e não-verbal.
+
+                  Os Textos estruturam-se de acordo com algumas regras, fatores e características:
+
+                  Contexto – como, com quem ocorrem as formas comunicativas; dados comuns ao emissor e ao receptor. Busca representar o ouvinte e o leitor, estabelecer o lugar que acorre a interação e quais finalidades.
+
+                  Intertextualidade – é ser capaz de observar ou fazer ligações de um texto sobre outro, seja ou não do mesmo autor, porém de mesmo assunto. Quando lemos ou ouvimos um texto é necessária uma série de informações externas para um melhor entendimento do texto.
+              </p>
+              </div>
+            </div>
+          </div>
+        </section>
+     </main>
+
+
+
+       <footer class="text-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-4">
+              <h5>Menu</h5>
+              <ul>
+                <li><a href="page2.php">Page 2</a></li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+              </ul>
+            </div>
+            <div class="col-4">
+              <h5>Informações</h5>
+              <ul>
+                <li><a href="page2.php">Page 2</a></li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+              </ul>
+            </div>
+            <div class="col-4">
+              <h5>Redes Sociais</h5>
+              <div class="row">
+                <div class="col-6">
+                  <i class="fa fa-instagram float-right" aria-hidden="true"></i>
+                </div>
+                <div class="col-6">
+                  <i class="fa fa-facebook float-left" aria-hidden="true"></i>
+                </div>
+                <div class="col-6">
+                  <i class="fa fa-twitter float-right" aria-hidden="true"></i>
+                </div>
+                <div class="col-6">
+                  <i class="fa fa-google-plus float-left" aria-hidden="true"></i>
+                </div>
+              </div>
+            </div>
+          </div>     
+        </div>
+      </footer>
+    </div>
+
+    <script type="text/javascript" src="assets/libs/jquery/jquery-3.5.1.js"></script>
+    <script type="text/javascript" src="assets/libs/popper/popper.min.js"></script>
+    <script type="text/javascript" src="assets/libs/bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/site/site.js"></script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+<!--     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../../../dist/js/bootstrap.min.js"></script> -->
+  </body>
+</html>
